c03ed191ada9afaa1116058ea18fc5999d08d03c
William Chang <mr.williamchang@gmail.com>
2015-11-18 10:28:54 -0800

    Highlight buttons when selected

--- toolBar.react.js
+++ toolBar.react.js
@@ -1,5 +1,6 @@
 /* @flow */
 
+var classnames = require('classnames');
 var React = require('react');
 var { Dispatcher } = require('flux');
 
@@ -9,17 +10,27 @@ var ToolBar = React.createClass({
     dispatcher: React.PropTypes.instanceOf(Dispatcher)
   },
 
+  getInitialState: function() {
+    return {
+      currentTool: "paintbrush"
+    }
+  },
+
   render: function(): ?ReactElement {
     return (
       <div className="p4 flex flex-column border">
         <h1 className="mt2">Toolbar</h1>
         <div id="paintbrush"
-             className="mt2 btn btn-primary black bg-silver"
+             className={classnames("mt2", "btn", "btn-primary", "black",
+                                   {"bg-silver": this.state.currentTool != "paintbrush",
+                                    "bg-yellow": this.state.currentTool == "paintbrush"})}
              onClick={this.handleClick} >
           Paintbrush
         </div>
         <div id="floodfill"
-             className="mt2 btn black bg-silver"
+             className={classnames("mt2", "btn", "btn-primary", "black",
+                                   {"bg-silver": this.state.currentTool != "floodfill",
+                                    "bg-yellow": this.state.currentTool == "floodfill"})}
              onClick={this.handleClick} >
           Floodfill
         </div>
@@ -27,12 +38,22 @@ var ToolBar = React.createClass({
     )
   },
 
-  handleClick: function(event: SyntheticEvent) {
+  componentDidMount: function() {
+    // Happens after first full render of page
     this.context.dispatcher.dispatch({
       actionType: "status-update",
-      // $FlowIgnore: flow unable to infer that id is available on event.target
-      status: event.target.id,
+      status: this.state.currentTool
     })
+  },
+
+  handleClick: function(event: SyntheticEvent) {
+    // $FlowIgnore: flow unable to infer that id is available on event.target
+    var currentTool = event.target.id;
+    this.context.dispatcher.dispatch({
+      actionType: "status-update",
+      status: currentTool
+    })
+    this.setState({currentTool: currentTool});
   }
 })
 
